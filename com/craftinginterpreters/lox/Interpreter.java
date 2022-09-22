231d484921b37947cff1d187b23895f8057d0f5d
Aira Villapando <villapando.aira@gmail.com>
2022-09-22 10:47:27 +0800

    implemented ch9 until logical operators

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -11,6 +11,20 @@ class Interpreter implements Expr.Visitor<Object>,
     public Object visitLiteralExpr(Expr.Literal expr) {
       return expr.value;
     }
+
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+      Object left = evaluate(expr.left);
+  
+      if (expr.operator.type == TokenType.OR) {
+        if (isTruthy(left)) return left;
+      } else {
+        if (!isTruthy(left)) return left;
+      }
+  
+      return evaluate(expr.right);
+    }
+    
     // evaluating unary expressions
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
@@ -110,6 +124,16 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
       evaluate(stmt.expression);
+      return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+      if (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.thenBranch);
+      } else if (stmt.elseBranch != null) {
+        execute(stmt.elseBranch);
+      }
       return null;
     }
 
